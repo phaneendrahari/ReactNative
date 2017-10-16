4d0adecc7fe5bc06782e6943508ee88abe819446
Phaneendra Hari <phari@cognitivescale.com>
2017-10-16 15:16:26 +0530

    Adding a Header

--- App.js
+++ App.js
@@ -5,12 +5,13 @@
  */
 
 import React, { Component } from 'react';
+
 import {
   Platform,
-  StyleSheet,
-  Text,
-  View
+  StyleSheet
 } from 'react-native';
+
+import Header from './src/components/header';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -22,17 +23,7 @@ const instructions = Platform.select({
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <Header />
     );
   }
 }
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+// Import libraries for making a Component
+import React from 'react';
+import {
+    Text
+} from 'react-native';
+
+// We will make a Component
+const Header = () => {
+    return <Text>PrtyReg</Text>;
+};
+
+//Make the component available to other parts of the app
+export default Header;
